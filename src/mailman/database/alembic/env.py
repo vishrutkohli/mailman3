7d8c36da200d87a37250d4300b50a98602130614
Abhilash Raj <raj.abhilash1@gmail.com>
2014-09-24 23:15:48 +0530

    added license block for the new file

--- src/mailman/database/alembic/env.py
+++ src/mailman/database/alembic/env.py
@@ -1,4 +1,22 @@
+# Copyright (C) 2006-2014 by the Free Software Foundation, Inc.
+#
+# This file is part of GNU Mailman.
+#
+# GNU Mailman is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# GNU Mailman is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# GNU Mailman.  If not, see <http://www.gnu.org/licenses/>.
+
 from __future__ import with_statement
+
 from alembic import context
 from sqlalchemy import create_engine, pool
 from logging.config import fileConfig
